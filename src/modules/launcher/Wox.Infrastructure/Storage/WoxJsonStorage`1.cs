b921205bc2be687fdf37c7e2addc97f9a170c42e
ozsun88 <92741674+ozsun88@users.noreply.github.com>
2021-12-18 04:03:11 +0800

    Initialize users/mykhailo/net5-fixes

---
+++ src/modules/launcher/Wox.Infrastructure/Storage/WoxJsonStorage`1.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO.Abstractions;
+using Wox.Plugin;
+
+namespace Wox.Infrastructure.Storage
+{
+    public class WoxJsonStorage<T> : JsonStorage<T>
+        where T : new()
+    {
+        private static readonly IFileSystem FileSystem = new FileSystem();
+        private static readonly IPath Path = FileSystem.Path;
+
+        public WoxJsonStorage(string fileName = "")
+        {
+            var directoryPath = Path.Combine(Constant.DataDirectory, DirectoryName);
+            Helper.ValidateDirectory(directoryPath);
+
+            var filename = fileName != null && fileName.Length != 0 ? fileName : typeof(T).Name;
+            FilePath = Path.Combine(directoryPath, $"{filename}{FileSuffix}");
+        }
+    }
+}
