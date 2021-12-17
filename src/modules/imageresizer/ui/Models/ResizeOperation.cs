61ef0431defdf5e0b93ced1ad3dcb0cf34e817f9
ozsun88 <92741674+ozsun88@users.noreply.github.com>
2021-12-18 04:04:16 +0800

    Initialize stefan/performance_work

---
+++ src/modules/imageresizer/ui/Models/ResizeOperation.cs
@@ -0,0 +1,275 @@
+﻿// Copyright (c) Brice Lambson
+// The Brice Lambson licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.  Code forked from Brice Lambson's https://github.com/bricelam/ImageResizer/
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ImageResizer.Extensions;
+using ImageResizer.Properties;
+using ImageResizer.Utilities;
+using Microsoft.VisualBasic.FileIO;
+using FileSystem = Microsoft.VisualBasic.FileIO.FileSystem;
+
+namespace ImageResizer.Models
+{
+    internal class ResizeOperation
+    {
+        private readonly IFileSystem _fileSystem = new System.IO.Abstractions.FileSystem();
+
+        private readonly string _file;
+        private readonly string _destinationDirectory;
+        private readonly Settings _settings;
+
+        // Filenames to avoid according to https://docs.microsoft.com/en-us/windows/win32/fileio/naming-a-file#file-and-directory-names
+        private static readonly string[] _avoidFilenames =
+            {
+                "CON", "PRN", "AUX", "NUL",
+                "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+                "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
+            };
+
+        public ResizeOperation(string file, string destinationDirectory, Settings settings)
+        {
+            _file = file;
+            _destinationDirectory = destinationDirectory;
+            _settings = settings;
+        }
+
+        public void Execute()
+        {
+            string path;
+            using (var inputStream = _fileSystem.File.OpenRead(_file))
+            {
+                var decoder = BitmapDecoder.Create(
+                    inputStream,
+                    BitmapCreateOptions.PreservePixelFormat,
+                    BitmapCacheOption.None);
+
+                var encoder = BitmapEncoder.Create(decoder.CodecInfo.ContainerFormat);
+                if (!encoder.CanEncode())
+                {
+                    encoder = BitmapEncoder.Create(_settings.FallbackEncoder);
+                }
+
+                ConfigureEncoder(encoder);
+
+                if (decoder.Metadata != null)
+                {
+                    try
+                    {
+                        encoder.Metadata = decoder.Metadata;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                    }
+                }
+
+                if (decoder.Palette != null)
+                {
+                    encoder.Palette = decoder.Palette;
+                }
+
+                foreach (var originalFrame in decoder.Frames)
+                {
+                    BitmapMetadata metadata = (BitmapMetadata)originalFrame.Metadata;
+                    if (metadata != null)
+                    {
+                        try
+                        {
+                            // Detect whether metadata can copied successfully
+                            _ = metadata.Clone();
+                        }
+                        catch (ArgumentException)
+                        {
+                            metadata = null;
+                        }
+                    }
+
+                    if (_settings.RemoveMetadata && metadata != null)
+                    {
+                        // strip any metadata that doesn't affect rendering
+                        var newMetadata = new BitmapMetadata(metadata.Format);
+
+                        metadata.CopyMetadataPropertyTo(newMetadata, "System.Photo.Orientation");
+                        metadata.CopyMetadataPropertyTo(newMetadata, "System.Image.ColorSpace");
+
+                        metadata = newMetadata;
+                    }
+
+                    encoder.Frames.Add(
+                        BitmapFrame.Create(
+                            Transform(originalFrame),
+                            thumbnail: null,
+                            metadata,
+                            colorContexts: null));
+                }
+
+                path = GetDestinationPath(encoder);
+                _fileSystem.Directory.CreateDirectory(_fileSystem.Path.GetDirectoryName(path));
+                using (var outputStream = _fileSystem.File.Open(path, FileMode.CreateNew, FileAccess.Write))
+                {
+                    encoder.Save(outputStream);
+                }
+            }
+
+            if (_settings.KeepDateModified)
+            {
+                _fileSystem.File.SetLastWriteTimeUtc(path, _fileSystem.File.GetLastWriteTimeUtc(_file));
+            }
+
+            if (_settings.Replace)
+            {
+                var backup = GetBackupPath();
+                _fileSystem.File.Replace(path, _file, backup, ignoreMetadataErrors: true);
+                FileSystem.DeleteFile(backup, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+            }
+        }
+
+        private void ConfigureEncoder(BitmapEncoder encoder)
+        {
+            switch (encoder)
+            {
+                case JpegBitmapEncoder jpegEncoder:
+                    jpegEncoder.QualityLevel = MathHelpers.Clamp(_settings.JpegQualityLevel, 1, 100);
+                    break;
+
+                case PngBitmapEncoder pngBitmapEncoder:
+                    pngBitmapEncoder.Interlace = _settings.PngInterlaceOption;
+                    break;
+
+                case TiffBitmapEncoder tiffEncoder:
+                    tiffEncoder.Compression = _settings.TiffCompressOption;
+                    break;
+            }
+        }
+
+        private BitmapSource Transform(BitmapSource source)
+        {
+            var originalWidth = source.PixelWidth;
+            var originalHeight = source.PixelHeight;
+            var width = _settings.SelectedSize.GetPixelWidth(originalWidth, source.DpiX);
+            var height = _settings.SelectedSize.GetPixelHeight(originalHeight, source.DpiY);
+
+            if (_settings.IgnoreOrientation
+                && !_settings.SelectedSize.HasAuto
+                && _settings.SelectedSize.Unit != ResizeUnit.Percent
+                && originalWidth < originalHeight != (width < height))
+            {
+                var temp = width;
+                width = height;
+                height = temp;
+            }
+
+            var scaleX = width / originalWidth;
+            var scaleY = height / originalHeight;
+
+            if (_settings.SelectedSize.Fit == ResizeFit.Fit)
+            {
+                scaleX = Math.Min(scaleX, scaleY);
+                scaleY = scaleX;
+            }
+            else if (_settings.SelectedSize.Fit == ResizeFit.Fill)
+            {
+                scaleX = Math.Max(scaleX, scaleY);
+                scaleY = scaleX;
+            }
+
+            if (_settings.ShrinkOnly
+                && _settings.SelectedSize.Unit != ResizeUnit.Percent
+                && (scaleX >= 1 || scaleY >= 1))
+            {
+                return source;
+            }
+
+            var scaledBitmap = new TransformedBitmap(source, new ScaleTransform(scaleX, scaleY));
+            if (_settings.SelectedSize.Fit == ResizeFit.Fill
+                && (scaledBitmap.PixelWidth > width
+                || scaledBitmap.PixelHeight > height))
+            {
+                var x = (int)(((originalWidth * scaleX) - width) / 2);
+                var y = (int)(((originalHeight * scaleY) - height) / 2);
+
+                return new CroppedBitmap(scaledBitmap, new Int32Rect(x, y, (int)width, (int)height));
+            }
+
+            return scaledBitmap;
+        }
+
+        private string GetDestinationPath(BitmapEncoder encoder)
+        {
+            var directory = _destinationDirectory ?? _fileSystem.Path.GetDirectoryName(_file);
+            var originalFileName = _fileSystem.Path.GetFileNameWithoutExtension(_file);
+
+            var supportedExtensions = encoder.CodecInfo.FileExtensions.Split(',');
+            var extension = _fileSystem.Path.GetExtension(_file);
+            if (!supportedExtensions.Contains(extension, StringComparer.OrdinalIgnoreCase))
+            {
+                extension = supportedExtensions.FirstOrDefault();
+            }
+
+            // Remove directory characters from the size's name.
+            string sizeNameSanitized = _settings.SelectedSize.Name;
+            sizeNameSanitized = sizeNameSanitized
+                .Replace('\\', '_')
+                .Replace('/', '_');
+
+            // Using CurrentCulture since this is user facing
+            var fileName = string.Format(
+                CultureInfo.CurrentCulture,
+                _settings.FileNameFormat,
+                originalFileName,
+                sizeNameSanitized,
+                _settings.SelectedSize.Width,
+                _settings.SelectedSize.Height,
+                encoder.Frames[0].PixelWidth,
+                encoder.Frames[0].PixelHeight);
+
+            // Remove invalid characters from the final file name.
+            fileName = fileName
+                .Replace(':', '_')
+                .Replace('*', '_')
+                .Replace('?', '_')
+                .Replace('"', '_')
+                .Replace('<', '_')
+                .Replace('>', '_')
+                .Replace('|', '_');
+
+            // Avoid creating not recommended filenames
+            if (_avoidFilenames.Contains(fileName.ToUpperInvariant()))
+            {
+                fileName = fileName + "_";
+            }
+
+            var path = _fileSystem.Path.Combine(directory, fileName + extension);
+            var uniquifier = 1;
+            while (_fileSystem.File.Exists(path))
+            {
+                path = _fileSystem.Path.Combine(directory, fileName + " (" + uniquifier++ + ")" + extension);
+            }
+
+            return path;
+        }
+
+        private string GetBackupPath()
+        {
+            var directory = _fileSystem.Path.GetDirectoryName(_file);
+            var fileName = _fileSystem.Path.GetFileNameWithoutExtension(_file);
+            var extension = _fileSystem.Path.GetExtension(_file);
+
+            var path = _fileSystem.Path.Combine(directory, fileName + ".bak" + extension);
+            var uniquifier = 1;
+            while (_fileSystem.File.Exists(path))
+            {
+                path = _fileSystem.Path.Combine(directory, fileName + " (" + uniquifier++ + ")" + ".bak" + extension);
+            }
+
+            return path;
+        }
+    }
+}
---
+++ src/modules/previewpane/powerpreview/CLSID.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <guiddef.h>
+
+// 74619BDA-A66B-451D-864C-A7726F5FE650
+// CLSID used in manifest file for Preview Handler.
+const CLSID CLSID_SHIMActivateSvgPreviewHandler = { 0x74619BDA, 0xA66B, 0x451D, { 0x86, 0x4C, 0xA7, 0x72, 0x6F, 0x5F, 0xE6, 0x50 } };
+
+// ddee2b8a-6807-48a6-bb20-2338174ff779
+// CLSID of the .Net Com Class for Preview Handler. Should be included in the registry.dat file under \Classes\CLSID\{guid}.
+// More details here: https://docs.microsoft.com/en-us/dotnet/framework/interop/registering-assemblies-with-com
+const CLSID CLSID_SvgPreviewHandler = { 0xddee2b8a, 0x6807, 0x48a6, { 0xbb, 0x20, 0x23, 0x38, 0x17, 0x4f, 0xf7, 0x79 } };
+
+// E0907A95-6F9A-4D1B-A97A-7D9D2648881E
+const CLSID CLSID_SHIMActivateMdPreviewHandler = { 0xE0907A95, 0x6F9A, 0x4D1B, { 0xA9, 0x7A, 0x7D, 0x9D, 0x26, 0x48, 0x88, 0x1E } };
+
+// 45769bcc-e8fd-42d0-947e-02beef77a1f5
+const CLSID CLSID_MdPreviewHandler = { 0x45769bcc, 0xe8fd, 0x42d0, { 0x94, 0x7e, 0x02, 0xbe, 0xef, 0x77, 0xa1, 0xf5 } };
+
+// 4F6D533B-4185-43A6-AD75-9B20034B14CA
+const CLSID CLSID_SHIMActivatePdfPreviewHandler = { 0x4f6d533b, 0x4185, 0x43a6, { 0xad, 0x75, 0x9b, 0x20, 0x3, 0x4b, 0x14, 0xca } };
+
+// 07665729-6243-4746-95b7-79579308d1b2
+const CLSID CLSID_PdfPreviewHandler = { 0x07665729, 0x6243, 0x4746, { 0x95, 0xb7, 0x79, 0x57, 0x93, 0x08, 0xd1, 0xb2 } };
+
+// 9C723B8C-4F5C-4147-9DE4-C2808F9AF66B
+const CLSID CLSID_SHIMActivateSvgThumbnailProvider = { 0x9C723B8C, 0x4F5C, 0x4147, { 0x9D, 0xE4, 0xC2, 0x80, 0x8F, 0x9A, 0xF6, 0x6B } };
+
+// 36B27788-A8BB-4698-A756-DF9F11F64F84
+const CLSID CLSID_SvgThumbnailProvider = { 0x36B27788, 0xA8BB, 0x4698, { 0xA7, 0x56, 0xDF, 0x9F, 0x11, 0xF6, 0x4F, 0x84 } };
+
+// BCC13D15-9720-4CC4-8371-EA74A274741E
+const GUID CLSID_PdfThumbnailProvider = { 0xbcc13d15, 0x9720, 0x4cc4, { 0x83, 0x71, 0xea, 0x74, 0xa2, 0x74, 0x74, 0x1e } };
+ 
+// Pairs of NativeClsid vs ManagedClsid used for preview handlers.
+const std::vector<std::pair<CLSID, CLSID>> NativeToManagedClsid({ 
+    { CLSID_SHIMActivateMdPreviewHandler, CLSID_MdPreviewHandler },
+    { CLSID_SHIMActivatePdfPreviewHandler, CLSID_PdfPreviewHandler },
+    { CLSID_SHIMActivateSvgPreviewHandler, CLSID_SvgPreviewHandler },
+    { CLSID_SHIMActivateSvgThumbnailProvider, CLSID_SvgThumbnailProvider }
+});
